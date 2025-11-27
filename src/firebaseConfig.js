f2b0490d0c6bf505707d0797ab981c1356ee4534
BARSOUM johnny (PEpsi) <pepsijohnny22@gmail.com>
2025-11-27 02:03:32 +0200

    Update Firebase configuration to use module script

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBGMJIAVxZtfEnPlYxZQxOVSLUX4YGlmhs",
-  authDomain: "ppep-6cd6f.firebaseapp.com",
-  projectId: "ppep-6cd6f",
-  storageBucket: "ppep-6cd6f.firebasestorage.app",
-  messagingSenderId: "661866399642",
-  appId: "1:661866399642:web:605b28980a4a63569adc23"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGMJIAVxZtfEnPlYxZQxOVSLUX4YGlmhs",
+    authDomain: "ppep-6cd6f.firebaseapp.com",
+    projectId: "ppep-6cd6f",
+    storageBucket: "ppep-6cd6f.firebasestorage.app",
+    messagingSenderId: "661866399642",
+    appId: "1:661866399642:web:75566e442157271f9adc23"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
