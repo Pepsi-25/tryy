000ffdeea86087ece2fcd7e7f3df062971c0aa5f
BARSOUM johnny (PEpsi) <pepsijohnny22@gmail.com>
2025-11-27 03:02:22 +0200

    Update Firebase configuration and app initialization

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -11,10 +11,11 @@ const firebaseConfig = {
   projectId: "tryy-5ea43",
   storageBucket: "tryy-5ea43.firebasestorage.app",
   messagingSenderId: "259503180304",
-  appId: "1:259503180304:web:c0066e0b3c041efa439895"
+  appId: "1:259503180304:web:1bfab8eb93e6bb57439895"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+
 
